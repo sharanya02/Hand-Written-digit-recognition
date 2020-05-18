8e5358f48828c362cbadc08563a08e7d5e55d113
sharanya02 <59661067+sharanya02@users.noreply.github.com>
2020-05-18 14:44:59 +0530

    Add files via upload

---
+++ digitfinal.py
@@ -0,0 +1,32 @@
+#importing the dataset
+from sklearn.datasets import load_digits
+digits = load_digits()
+# Print to show there are 1797 images (8 by 8 images for a dimensionality of 64)
+print("Image Data Shape" , digits.data.shape)
+import numpy as np 
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(20,4))
+for index, (image, label) in enumerate(zip(digits.data[0:5], digits.target[0:5])):
+    plt.subplot(1, 5, index + 1)
+    plt.imshow(np.reshape(image, (8,8)), cmap=plt.cm.gray)
+    plt.title('Training: %i\n' % label, fontsize = 20)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.25, random_state=0)
+from sklearn.linear_model import LogisticRegression
+logisticRegr = LogisticRegression()
+logisticRegr.fit(x_train, y_train)
+predictions = logisticRegr.predict(x_test)
+score = logisticRegr.score(x_test, y_test)
+print(score)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
+cm = metrics.confusion_matrix(y_test, predictions)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Pastel1');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'Accuracy Score: {0}'.format(score)
+plt.title(all_sample_title, size = 15);
